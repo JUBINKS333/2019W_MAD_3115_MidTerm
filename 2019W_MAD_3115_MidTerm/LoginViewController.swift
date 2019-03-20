ab8b0f6e6d1152a3079887310f3cd7b5c6c7b15c
MacStudent <macstudent@126-iMac38.local>
2019-03-20 18:45:46 -0400

    initial

--- 2019W_MAD_3115_MidTerm/LoginViewController.swift
+++ 2019W_MAD_3115_MidTerm/LoginViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+   
+
+
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
 
 
+
+    
+    
 }
 
